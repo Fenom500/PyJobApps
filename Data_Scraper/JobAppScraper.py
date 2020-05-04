b247eebe22c97b3701e7d8a25907e2db74174fa9
Shahil Rauf <Shahilzr@gmail.com>
2020-05-03 18:17:55 -0700

    Update JobAppScraper.py

--- Data_Scraper/JobAppScraper.py
+++ Data_Scraper/JobAppScraper.py
@@ -28,9 +28,20 @@ class SearchResults(WebPage):
 
     def __init__(self, target_link):
         super().__init__(target_link)
-
+        pass
+    #     self.job_posts = self.soup.find_all("a", class_="job_link t_job_link")
+    #     self.post_links = []
+    #     self.get_links()
+    #
+    # def get_links(self):
+    #     for post in self.job_posts:
+    #         print(post.text)
 
 
 # sample_job_1 = "https://www.ziprecruiter.com/jobs/vector-atomic-63c83f51/embedded-programmer-entry-level-7dc73e97?job_id=0655f0a8b0ae0725fc4e5e4553a3201b"
 # sample = JobPosting(sample_job_1)
 # print(sample.job_title)
+
+sample_search_results = "https://www.ziprecruiter.com/search?form=jobs-landing&search=entry+python&location=Hayward%2C+CA"
+sample = WebPage(sample_search_results)
+print(sample.soup)
